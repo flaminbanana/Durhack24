e93ece535e7c475ea04b0a9cda0de79041a55b14
RyanChong0117 <150850349+RyanChong0117@users.noreply.github.com>
2024-11-02 20:43:48 +0000

    Created structure for each category

--- DashboardApp/dashboard.py
+++ DashboardApp/dashboard.py
@@ -2,23 +2,20 @@ from tkinter import *
 from tkinter import ttk
 
 def search():
+	main_page.pack_forget()
 	#display the appropriate frame based on the category
-	main.pack_forget()
-	hotel_frame.pack(side=BOTTOM, fill=BOTH, expand=False)
+	
 
 root = Tk()
 root.title("Dashboard App")
 root.geometry("1600x900")
-root.resizable(True, True)
+root.resizable(False, False)
 
 
 header = Frame(root, height=150, bg = 'white')
 header.pack(side=TOP, fill=BOTH, expand=False)
 
-main = Frame(root, height = 2000, bg = 'lavender')
-main.pack(side=BOTTOM, fill=BOTH, expand=False)
-
-label = Label(header, text='Website', width=25, font="Arial, 60", bg="white", fg="black")
+label = Label(header, text='App', width=25, font="Arial, 60", bg="white", fg="black")
 label.place(x = 200, y = 75, anchor = CENTER)
 
 location_text = Label(header, text='Choose a location: ', font='Arial, 20', bg='white')
@@ -40,24 +37,43 @@ chosen_category.place(x=1300, y=75, anchor = CENTER)
 search_button = Button(header, text = 'Search', font = "Arial, 20", command = search)
 search_button.place(x=1500, y=75, anchor = CENTER)
 
+main_page = Frame(root, height = 2000, bg = 'lavender')
+main_page.pack(side=BOTTOM, fill=BOTH, expand=False)
+
+welcome_text = Label(main_page, text = 'Welcome to our application!', bg = 'lavender', font = "Arial, 30")
+welcome_text.place(x = 800, y = 200, anchor = CENTER)
+
+instruction_text = Label(main_page, text = 'Please choose a location and category above.', bg = 'lavender', font = "Arial, 20")
+instruction_text.place(x = 800, y = 250, anchor = CENTER)
+
+
+
 hotel_frame = Frame(root, height = 2000, bg = 'lavender')
 hotel_name_label = Label(hotel_frame, text = 'Hotel Name', bg = 'lavender', font = "Arial, 20")
 hotel_name_label.place(x = 500, y = 50, anchor=CENTER)
 hotel_price_label = Label(hotel_frame, text = 'Price (per night)', bg = 'lavender', font = "Arial, 20")
 hotel_price_label.place(x = 1100, y = 50, anchor=CENTER)
+#hotel_frame.pack(side=BOTTOM, fill=BOTH, expand=False)
 
 restaurant_frame = Frame(root, height = 2000, bg = 'lavender')
-restaurant_name_label = Label(restaurant_frame, text = 'Restaurant Name')
-restaurant_rating_label = Label(restaurant_frame, text = 'Rating')
-restaurant_review_count_label = Label(restaurant_frame, text = 'Number of Reviews')
-avg_spending_pp_label = Label(restaurant_frame, text = 'Average spending (per person)')
+restaurant_name_label = Label(restaurant_frame, text = 'Restaurant Name', bg = 'lavender', font = "Arial, 20")
+restaurant_name_label.place(x = 200, y = 50, anchor=CENTER)
+restaurant_rating_label = Label(restaurant_frame, text = 'Rating', bg = 'lavender', font = "Arial, 20")
+restaurant_rating_label.place(x = 550, y = 50, anchor=CENTER)
+restaurant_review_count_label = Label(restaurant_frame, text = 'Number of Reviews', bg = 'lavender', font = "Arial, 20")
+restaurant_review_count_label.place(x = 900, y = 50, anchor=CENTER)
+restaurant_review_quote_label = Label(restaurant_frame, text = 'Average spending (per person)', bg = 'lavender', font = "Arial, 20")
+restaurant_review_quote_label.place(x = 1300, y = 50, anchor=CENTER)
 #restaurant_frame.pack(side=BOTTOM, fill=BOTH, expand=False)
 
 attractions_frame = Frame(root, height = 2000, bg = 'lavender')
-attractions_name_label = Label(attractions_frame, text = 'Attraction Name')
-attractions_rating_label = Label(attractions_frame, text = 'Rating')
-attractions_review_count_label = Label(attractions_frame, text = 'Number of Reviews')
-attraction_price_label = Label(attractions_frame, text = 'Admission fee')
-#attractions_frame.pack(side=BOTTOM, fill=BOTH, expand=False)
+attractions_name_label = Label(attractions_frame, text = 'Attraction Name', bg = 'lavender', font = "Arial, 20")
+attractions_name_label.place(x = 200, y = 50, anchor=CENTER)
+attractions_rating_label = Label(attractions_frame, text = 'Rating', bg = 'lavender', font = "Arial, 20")
+attractions_rating_label.place(x = 600, y = 50, anchor=CENTER)
+attractions_review_count_label = Label(attractions_frame, text = 'Number of Reviews', bg = 'lavender', font = "Arial, 20")
+attractions_review_count_label.place(x = 1000, y = 50, anchor=CENTER)
+attraction_price_label = Label(attractions_frame, text = 'Admission fee', bg = 'lavender', font = "Arial, 20")
+attraction_price_label.place(x = 1400, y = 50, anchor=CENTER)
 
 root.mainloop()
